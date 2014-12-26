0eb1a1583834471a0089f51d461b5e4185012727
vorobey <kennyx46@gmail.com>
2014-12-26 20:31:22 +0200

    signup form

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ SampleApp::Application.routes.draw do
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
 
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
